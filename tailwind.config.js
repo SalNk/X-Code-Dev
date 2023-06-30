910fd99767aabbcd556476fb79ea10a525b0a73d
SalNk <salemnk02@gmail.com>
2023-06-30 16:38:04 +0200

    recustomiser

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,59 @@ module.exports = {
   content: ['./index.html'],
   theme: {
     extend: {
+      darkMode: 'class',
+
       colors: {
         "blue-bg": "#010E4C",
         "blue-text": "#3EBFF8",
         "blue-clair": "#67CCFA",
         "who-bg": "#051562",
-        "block-bg": "#3552CF"
+        "block-bg": "#3552CF",
+        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a", "950": "#172554" }
+
       },
       fontFamily: {
         poppins: ["poppins"],
+        'body': [
+          'Inter',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'system-ui',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji'
+        ],
+        'sans': [
+          'Inter',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'system-ui',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji'
+        ]
       },
       width: {
         '40': '30%'
       }
     }
   },
-  plugins: [],
+  plugins: []
+
 }
